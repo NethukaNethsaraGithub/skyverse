d22014ad061e74b6e2d31fcd7218d1afd136e2e1
Nethuka <158580923+NethukaNethsaraGithub@users.noreply.github.com>
2024-05-17 16:58:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ cd "$plugins_dir"
 
 # Download HibernateX.jar
 echo "Downloading Dependencies..."
-curl -s -L -o "HibernateX.jar" "https://github.com/alex1028199/artixegg/releases/download/Hiber/HibernateX.jar" > /dev/null
+curl -s -L -o "HibernateX.jar" "https://github.com/nethukanethsaragithub/skyverse/releases/download/Spark/spark.jar" > /dev/null
 if [ -f "HibernateX.jar" ]; then
     echo "HibernateX.jar downloaded successfully."
 else
@@ -23,14 +23,14 @@ else
 fi
 
 # Download Spark.jar
-curl -s -L -o "Spark.jar" "https://github.com/alex1028199/artixegg/releases/download/Spark/spark.jar" > /dev/null
+curl -s -L -o "Spark.jar" "https://github.com/nethukanethsaragithub/skyverse/releases/download/Spark/spark.jar" > /dev/null
 if [ -f "Spark.jar" ]; then
     echo "Spark.jar downloaded successfully."
 else
     echo "Failed to download Spark.jar."
 fi
 
-echo "THIS SYSTEM IS RUNNING BY SPARIXXNODES!"
+echo "THIS SYSTEM IS RUNNING BY SKY VERSE UWU"
 # Run Java command
 cd ..
 java -Xms8192M -Xmx8192M -Xdump:none -jar server.jar
